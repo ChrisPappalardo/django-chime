2d2a2bb34e44cc6ffb4da5d6afdd03d2aa7903d3
Chris Pappalardo <cpappala@gmail.com>
2020-04-18 12:34:42 -0700

    changed loglevel of penn_chime models logger to WARNING

--- django_chime/__init__.py
+++ django_chime/__init__.py
@@ -4,6 +4,13 @@
 Top-level package for django-chime.
 '''
 
+import logging
+
+
 __author__ = 'Chris Pappalardo'
 __email__ = 'cpappala@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
+
+
+# set penn_chime log level to something less chatty
+logging.getLogger('penn_chime.models').setLevel(logging.WARNING)
